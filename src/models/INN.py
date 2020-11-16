b66c67cf377638f3ee997a48927e3fd7a706465c
Franziska <franziska.schwaiger@fraunhofer.iks.de>
2020-11-16 20:06:21 +0100

    Basic structure added

---
+++ src/models/INN.py
@@ -0,0 +1,16 @@
+import torch
+from torch import nn
+
+class INN(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+        # TODO: Build architecture
+
+
+    def forward(self, X):
+
+        # TODO: Implement forward method
+
+        return X
---
+++ src/train.py
@@ -0,0 +1,8 @@
+import torch
+
+from models import AE
+
+if __name__ == '__main__':
+
+    # simple_AE = AE().cpu()
+    pass
