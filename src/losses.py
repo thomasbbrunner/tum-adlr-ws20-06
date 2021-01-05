kl vanishing problem fixed nan problem fixed

src/losses.py
@@ -109,41 +109,38 @@ def VAE_loss_ROBOT_SIM(recon_x, x, mu, logvar, variational_beta):
 
 # Input x: gt input vector with shape: num_samples x 2 * num_joints
 # Input _x: predicted input vector with shape: num_samples x 2 * num_joints
-def custom_loss(_x, x):
+def custom_loss(_x, x, reduction):
 
     num_joints = int(x.size()[1] / 2)
     samples = x.size()[0]
-
-    # normalize vectors
-    # _x_normalized = normalize(_x)
 
     # vectorize input vectors
     _x_vectorized = vectorize(_x)
     x_vectorized = vectorize(x)
 
     # normalize preds direction vectors
-    _x_normalized = nn.functional.normalize(input=_x_vectorized, dim=2) # eps=0.00001
+    # eps important in order to avoid dividing by 0
+    _x_normalized = nn.functional.normalize(input=_x_vectorized, p=2, dim=2, eps=1e-8)
+    if torch.any(torch.isnan(_x_normalized)):
+        print(_x_normalized)
+        raise Exception('NaN in _x_normalized detected!')
 
-
-    # _x_clipped = torch.clip(input=_x_vectorized, min=-1.00, max=1.00)
-    # x_clipped = torch.clip(input=x_vectorized, min=-1.00, max=1.00)
-
-    # print(_x_normalized)
-
-    loss_i = torch.zeros(size=(samples,))
     ones = torch.ones(size=(samples,))
+    loss_i = 0.0
     for i in range(num_joints):
-        # print('size of _x_vectorized:', _x_vectorized.size())
-        # print('size of ones:', ones.size())
-        # print('size of _x_vectorized[:, i, 0]:', _x_vectorized[:, i, 0].size())
-        # print(ones - (_x_vectorized[:, i, 0] * x_vectorized[:, i, 0] + _x_vectorized[:, i, 1] * x_vectorized[:, i, 1]))
-        loss_i += ones - (_x_normalized[:, i, 0] * x_vectorized[:, i, 0] + _x_normalized[:, i, 1]
-                                           * x_vectorized[:, i, 1])
+        dot_product = _x_normalized[:, i, 0] * x_vectorized[:, i, 0] + _x_normalized[:, i, 1] * x_vectorized[:, i, 1]
+        loss_i += MSE(dot_product, ones, reduction=reduction)
+        if torch.isnan(loss_i):
+            raise Exception('NaN in loss_i detected!')
 
-    # divide by num_joints ?
-    # loss_i /= num_joints
+    # divide by num_joints
+    loss_i /= num_joints
 
-    return torch.mean(loss_i)
+    return loss_i
+
+def custom_loss2(_x, x):
+    loss = MSE(_x, x, reduction='mean')
+    return loss
 
 # Testing example
 if __name__ == '__main__':

src/models/INN.py
@@ -151,8 +151,8 @@ class INN(nn.Module):
 
 
         else:
-            x = self.block5(x, inverse)
             # x = self.block6(x, inverse)
+            x = self.block5(x, inverse)
             # x = self.block5(self.perm5(x, inverse), inverse)
             x = self.block4(self.perm4(x, inverse), inverse)
             x = self.block3(self.perm3(x, inverse), inverse)

src/train_loader.py
@@ -56,9 +56,12 @@ def train_CVAE(model, config, dataloader, device):
 
             # compute losses
             # recon_loss = MSE(image_batch_recon, joint_batch, reduction='sum')
-            recon_loss = custom_loss(image_batch_recon, joint_batch)
+
+            # very important: reduction='sum', NOT 'mean' !!
+            recon_loss = custom_loss(image_batch_recon, joint_batch, reduction='sum')
             kldivergence = KL_divergence(latent_mu, latent_logvar)
 
+            # loss = recon_loss + nn.functional.sigmoid(epoch / config['num_epochs'] - 0.5) * kldivergence # config['variational_beta'] * kldivergence
             loss = recon_loss + config['variational_beta'] * kldivergence
 
             # backpropagation
@@ -85,8 +88,8 @@ def train_CVAE(model, config, dataloader, device):
         recon_loss_avg[-1] /= num_batches
         kl_loss_avg[-1] /= num_batches
 
-        print('Epoch [%d / %d] avg reconstruction error: %f, weighted avg kl error: %f, avg overall error: %f'
-              % (epoch + 1, config['num_epochs'], recon_loss_avg[-1], config['variational_beta'] * kl_loss_avg[-1],
+        print('Epoch [%d / %d] avg reconstruction error: %f, avg kl error: %f, avg overall error: %f'
+              % (epoch + 1, config['num_epochs'], recon_loss_avg[-1], kl_loss_avg[-1],
                  train_loss_avg[-1]))
 
 
@@ -163,6 +166,8 @@ def train_INN(model, config, dataloader, device):
         train_loss_Lx_avg_unweighted.append(0)
         num_batches = 0
 
+        reduction = 'sum'
+
         # If MMD on x-space is present from the start, the model can get stuck.
         # Instead, ramp it up exponentially.
         # loss_factor = min(1., 2. * 0.002 ** (1. - (float(epoch) / config['num_epochs'])))
@@ -219,7 +224,7 @@ def train_INN(model, config, dataloader, device):
             y_short = torch.cat((y[:, :config['latent_dim']], y[:, -config['output_dim']:]), dim=1)
             output_short = torch.cat((output[:, :config['latent_dim']], output[:, -config['output_dim']:].data), dim=1)
 
-            L_y = config['weight_Ly'] * MSE(output[:, config['latent_dim']:], y[:, config['latent_dim']:], reduction='mean')
+            L_y = config['weight_Ly'] * MSE(output[:, config['latent_dim']:], y[:, config['latent_dim']:], reduction=reduction)
             # print('L_y: ', config['weight_Ly'] * L_y)
             L_z = config['weight_Lz'] * MMD(output_short, y_short, device)
             # print('L_z: ', config['weight_Lz'] * L_z)
@@ -252,7 +257,7 @@ def train_INN(model, config, dataloader, device):
 
             # forces padding dims to be ignored
             # L_xy = config['weight_Lxy'] * MSE(output_inv, x, reduction='mean')
-            L_xy = config['weight_Lxy'] * custom_loss(output_inv, x)
+            L_xy = config['weight_Lxy'] * custom_loss(output_inv, x, reduction=reduction)
             # print('L_xy: ', config['weight_Ly'] * L_xy)
 
             # TODO: What is the benefit of that loss?
@@ -271,10 +276,26 @@ def train_INN(model, config, dataloader, device):
             optimizer.step()
 
             train_loss_avg[-1] += loss
+
+            # if torch.isnan(train_loss_avg[-1]):
+            #     raise Exception('NaN in loss detected!')
+
             train_loss_Ly_avg[-1] += L_y.data.detach()
+            if torch.isnan(train_loss_Ly_avg[-1]):
+                raise Exception('NaN in Ly loss detected!')
+
             train_loss_Lz_avg[-1] += L_z.data.detach()
+            if torch.isnan(train_loss_Lz_avg[-1]):
+                raise Exception('NaN in Lz loss detected!')
+
             train_loss_Lx_avg[-1] += L_x.data.detach()
+            if torch.isnan(train_loss_Lx_avg[-1]):
+                raise Exception('NaN in Lx loss detected!')
+
             train_loss_Lxy_avg[-1] += L_xy.data.detach()
+            if torch.isnan(train_loss_Lxy_avg[-1]):
+                raise Exception('NaN in Lxy loss detected!')
+
             train_loss_Lx_avg_unweighted[-1] += UNWEIGHTED_LOSS.data.detach()
 
             num_batches += 1
@@ -300,8 +321,6 @@ def train_INN(model, config, dataloader, device):
               'weighted average x-MSE loss: %f, weighted average x-MMD loss: %f, Overall average loss: %f'
               % (epoch + 1, config['num_epochs'], train_loss_Ly_avg[-1],
                                           train_loss_Lz_avg[-1], train_loss_Lxy_avg[-1], train_loss_Lx_avg[-1], train_loss_avg[-1]))
-
-        # print('Unweighted x-MMD loss: %f' % train_loss_Lx_avg_unweighted[-1])
 
     plt.title('AVG LOSS HISTORY')
     plt.xlabel('EPOCHS')