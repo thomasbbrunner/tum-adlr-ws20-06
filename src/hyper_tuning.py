c993922bdbfe0840a296ba6b1f99cd43c5aa8b12
Franziska Schwaiger <ga59mut@mytum.de>
2021-01-27 21:38:21 +0100

    tune package fpr hyperparameter tuning added

---
+++ src/hyper_tuning.py
@@ -0,0 +1,9 @@
+from ray import tune
+
+if __name__ == '__main__':
+    """Performs hyperparameter tuning for INN and CVAE
+    
+    Source: https://docs.ray.io/en/master/tune/
+    """
+
+    print('TUNING ...')
