b5d941f75d7bf19b326c4958599303b88c745a4f
Franziska Schwaiger <ga59mut@mytum.de>
2021-02-05 19:39:32 +0100

    minor changes

---
+++ src/visualize_dataset.py
@@ -0,0 +1,29 @@
+import torch
+from torch.utils.data import DataLoader
+import argparse
+
+from train_loader import *
+from models import *
+
+if __name__ == '__main__':
+
+    ####################################################################################################################
+    # TO MODIFY
+    ####################################################################################################################
+
+    DATASET_SAMPLES = 1e6
+    NORMAL = False
+    DOF = 20
+    LINKS = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1.0]
+    name = "20dof"
+
+    ####################################################################################################################
+
+    robot = robotsim.Planar(DOF, LINKS)
+    dataset = RobotSimDataset(robot, DATASET_SAMPLES, normal=NORMAL)
+
+    dataset.plot_configurations(
+        path="./figures/test_dataset/_{}_configs".format(name))
+    dataset = RobotSimDataset(robot, DATASET_SAMPLES)
+    dataset.histogram(
+        path="./figures/test_dataset/_{}_histogram".format(name))
